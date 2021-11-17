baab3e907bdd89729b94d4795583085525ac8162
trand <trand@192.168.1.7>
2021-11-17 23:07:13 +0700

    Khởi tạo project

---
+++ LTNC_Java/src/main/java/com/LTNC/LTNC_Java/LtncJavaApplication.java
@@ -0,0 +1,13 @@
+package com.LTNC.LTNC_Java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LtncJavaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LtncJavaApplication.class, args);
+	}
+
+}
---
+++ LTNC_Java/src/test/java/com/LTNC/LTNC_Java/LtncJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.LTNC.LTNC_Java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LtncJavaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
